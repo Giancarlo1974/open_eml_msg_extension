2e8a021e95c6bd57d6da95ce07b0813c6fed85c1
Marco Colloca <154096958+MarcoColloca@users.noreply.github.com>
2025-07-02 16:33:46 +0200

    Add → Update content.js 2

--- content.js
+++ content.js
@@ -1,20 +1,40 @@
-document.addEventListener('mousedown', function (e) {
+document.addEventListener('mousedown', async function (e) {
+
   const link = e.target.closest('a');
   if (!link || !link.href) return;
 
-  const href = link.href;
+  const url = link.href;
+  // console.log(url);
 
-  if (href.match(/\.(eml|msg)(\?.*)?$/i)) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
+  try {
+    const res = await fetch(url, {
+      method: 'HEAD',
+      mode: 'cors'
+    });
 
-    console.log('[Open EML & MSG] Clicked link:', href);
+    const contentType = res.headers.get('content-type');
+    // console.log('Content-Type:', contentType);
 
-    const encoded = encodeURIComponent(href);
-    const customUrl = `emlopen://${encoded}`;
+    if (url.match(/\.(eml|msg)(\?.*)?$/i)) {
+      e.preventDefault();
+      e.stopImmediatePropagation();
 
-    console.log('[Open EML & MSG] Redirecting to custom protocol:', customUrl);
+      // console.log('[Open EML & MSG] Clicked link:', href);
 
-    window.location.href = customUrl;
+      const encoded = encodeURIComponent(href);
+      const customUrl = `emlopen://${encoded}`;
+
+      // console.log('[Open EML & MSG] Redirecting to custom protocol:', customUrl);
+
+      window.location.href = customUrl;
+    } else if (contentType && contentType.includes('application/vnd.ms-outlook')) {
+      e.preventDefault()
+      e.stopImmediatePropagation();
+      const customUrl = url.replace('https', 'emlopen')
+      window.location.href = customUrl;
+    }
+
+  } catch (err) {
+    console.error('Errore nella richiesta HEAD:', err);
   }
 }, true);
